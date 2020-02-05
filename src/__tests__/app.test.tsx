61ed2bb8e63a3448b1f8593dbb5c90f27b716103
robert <dungeonry@hotmail.com>
2020-02-05 04:16:21 -0800

    circle ci config and a dummy test that takes forever to run.

---
+++ src/__tests__/app.test.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { render, fireEvent, screen } from "@testing-library/react";
+import ReactDOM from "react-dom";
+
+describe("hello", () => {
+  test("asdf", done => {
+    expect(true).toBe(true);
+    done();
+  });
+});
