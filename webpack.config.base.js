ac75a576be5c991eeb522a4dfff06fceca816c80
robert <dungeonry@hotmail.com>
2020-02-19 02:14:16 -0800

    turns out I forgot to remove the references to 'copy-webpack-plugin' and the last five releases have not been the latest release.

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -1,6 +1,5 @@
 const path = require("path");
 const HTMLWebpackPlugin = require("html-webpack-plugin");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
 const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
 
 module.exports = {
